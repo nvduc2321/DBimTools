f472ac76243459233a60a9e3f0c57580af7f9279
nvduc2321 <nvduc.gpt@gmail.com>
2025-05-01 22:39:33 +0700

    update

--- DBimTool/DBimTool/Tools/CreateRebarForMainHoleType1/services/MainHole1Wall1Service.cs
+++ DBimTool/DBimTool/Tools/CreateRebarForMainHoleType1/services/MainHole1Wall1Service.cs
@@ -1,37 +1,253 @@
-﻿using DBimTool.Tools.CreateRebarForMainHoleType1.iservices;
+﻿using Autodesk.Revit.DB.Structure;
+using DBimTool.Tools.CreateRebarForMainHoleType1.iservices;
 using DBimTool.Tools.CreateRebarForMainHoleType1.models;
 using DBimTool.Utils.Geometries;
+using DBimTool.Utils.Messages;
 using DBimTool.Utils.NumberUtils;
+using DBimTool.Utils.RevitElements;
 
 namespace DBimTool.Tools.CreateRebarForMainHoleType1.services
 {
     public class MainHole1Wall1Service : IMainHole1Wall1Service
     {
-        private CreateRebarForMainHoleType1ElementInstance _elementInstances;
-        private List<Line> _lineVerticalCenterLineRebar;
-        private List<Line> _lineHorizontalCenterLineRebar;
-        public MainHole1Wall1Service(CreateRebarForMainHoleType1ElementInstance elementInstances)
+        private CreateRebarForMainHoleType1Cmd _cmd;
+        private CreateRebarForMainHoleType1ElementInstance _elementInstance;
+        public MainHole1Wall1Service(
+            CreateRebarForMainHoleType1Cmd cmd,
+            CreateRebarForMainHoleType1ElementInstance elementInstances)
         {
-            _elementInstances = elementInstances;
+            _cmd = cmd;
+            _elementInstance = elementInstances;
+        }
+        public void InstallRebarVerticalNear()
+        {
+            try
+            {
+                var host = _cmd.Document.CreateHost(BuiltInCategory.OST_StructuralFoundation);
+                var wall = _elementInstance.MainHole1Wall1Rebar;
+                var cover = _elementInstance.CoverMm.MmToFoot();
+                var diameterType = _elementInstance.RevDiameters
+                    .FirstOrDefault(x => x.Name == wall.RebarVerticalNear.NameDiameter);
+                var diameter = diameterType.BarModelDiameter;
+                var normal = wall.HostInfo.VtX;
+                foreach (var l in wall.LineCenterVertical)
+                {
+                    try
+                    {
+                        var dir = l.Direction.Normalize();
+                        var p1 = l.GetEndPoint(0)
+                            - wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            + dir * (cover + diameter / 2);
+                        var p2 = l.GetEndPoint(0)
+                            + wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            + dir * (cover + diameter / 2);
+                        var p3 = l.GetEndPoint(1)
+                            + wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            - dir * (cover + diameter / 2);
+                        var p4 = l.GetEndPoint(1)
+                            - wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            - dir * (cover + diameter / 2);
+                        var ps = new List<XYZ>() { p1, p2, p3, p4 };
+                        var cs = ps.PointsToCurves();
+                        var rebar = Rebar.CreateFromCurves(
+                            _cmd.Document,
+                            RebarStyle.Standard,
+                            diameterType,
+                            null,
+                            null,
+                            host,
+                            normal,
+                            cs,
+                            RebarHookOrientation.Left,
+                            RebarHookOrientation.Left,
+                            true, true);
+
+                        if (_cmd.Document.ActiveView is View3D view3d)
+                            rebar.SetSolidInView(view3d, true);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                IO.ShowWarning(ex.Message);
+            }
+        }
+        public void InstallRebarHorizontalNear()
+        {
+            try
+            {
+                var host = _cmd.Document.CreateHost(BuiltInCategory.OST_StructuralFoundation);
+                var wall = _elementInstance.MainHole1Wall1Rebar;
+                var cover = _elementInstance.CoverMm.MmToFoot();
+                var diameterType = _elementInstance.RevDiameters
+                    .FirstOrDefault(x => x.Name == wall.RebarHorizontalNear.NameDiameter);
+                var diameterTypeVer = _elementInstance.RevDiameters
+                    .FirstOrDefault(x => x.Name == wall.RebarVerticalNear.NameDiameter);
+                var diameter = diameterType.BarModelDiameter;
+                var diameterVer = diameterTypeVer.BarModelDiameter;
+                var normal = wall.HostInfo.VtZ;
+                foreach (var l in wall.LineCenterHorizontal)
+                {
+                    try
+                    {
+                        var dir = l.Direction.Normalize();
+                        var p1 = l.GetEndPoint(0)
+                            - wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            + dir * (cover + diameter / 2);
+                        var p2 = l.GetEndPoint(0)
+                            + wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2 - diameterVer)
+                            + dir * (cover + diameter / 2);
+                        var p3 = l.GetEndPoint(1)
+                            + wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2 - diameterVer)
+                            - dir * (cover + diameter / 2);
+                        var p4 = l.GetEndPoint(1)
+                            - wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            - dir * (cover + diameter / 2);
+                        var ps = new List<XYZ>() { p1, p2, p3, p4 };
+                        var cs = ps.PointsToCurves();
+                        var rebar = Rebar.CreateFromCurves(
+                            _cmd.Document,
+                            RebarStyle.Standard,
+                            diameterType,
+                            null,
+                            null,
+                            host,
+                            normal,
+                            cs,
+                            RebarHookOrientation.Left,
+                            RebarHookOrientation.Left,
+                            true, true);
+
+                        if (_cmd.Document.ActiveView is View3D view3d)
+                            rebar.SetSolidInView(view3d, true);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                IO.ShowWarning(ex.Message);
+            }
+        }
+        public void InstallRebarVerticalFar()
+        {
+            try
+            {
+                var host = _cmd.Document.CreateHost(BuiltInCategory.OST_StructuralFoundation);
+                var wall = _elementInstance.MainHole1Wall1Rebar;
+                var cover = _elementInstance.CoverMm.MmToFoot();
+                var diameterType = _elementInstance.RevDiameters
+                    .FirstOrDefault(x => x.Name == wall.RebarVerticalFar.NameDiameter);
+                var diameter = diameterType.BarModelDiameter;
+                var normal = wall.HostInfo.VtX;
+                foreach (var l in wall.LineCenterVertical)
+                {
+                    try
+                    {
+                        var dir = l.Direction.Normalize();
+                        var p1 = l.GetEndPoint(0)
+                            + wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            + dir * (cover + diameter / 2);
+                        var p2 = l.GetEndPoint(0)
+                            - wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            + dir * (cover + diameter / 2);
+                        var p3 = l.GetEndPoint(1)
+                            - wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            - dir * (cover + diameter / 2);
+                        var p4 = l.GetEndPoint(1)
+                            + wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            - dir * (cover + diameter / 2);
+                        var ps = new List<XYZ>() { p1, p2, p3, p4 };
+                        var cs = ps.PointsToCurves();
+                        var rebar = Rebar.CreateFromCurves(
+                            _cmd.Document,
+                            RebarStyle.Standard,
+                            diameterType,
+                            null,
+                            null,
+                            host,
+                            normal,
+                            cs,
+                            RebarHookOrientation.Left,
+                            RebarHookOrientation.Left,
+                            true, true);
+
+                        if (_cmd.Document.ActiveView is View3D view3d)
+                            rebar.SetSolidInView(view3d, true);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                IO.ShowWarning(ex.Message);
+            }
         }
         public void InstallRebarHorizontalFar()
         {
-            throw new NotImplementedException();
-        }
+            try
+            {
+                var host = _cmd.Document.CreateHost(BuiltInCategory.OST_StructuralFoundation);
+                var wall = _elementInstance.MainHole1Wall1Rebar;
+                var cover = _elementInstance.CoverMm.MmToFoot();
+                var diameterType = _elementInstance.RevDiameters
+                    .FirstOrDefault(x => x.Name == wall.RebarHorizontalFar.NameDiameter);
+                var diameterTypeVer = _elementInstance.RevDiameters
+                    .FirstOrDefault(x => x.Name == wall.RebarVerticalFar.NameDiameter);
+                var diameter = diameterType.BarModelDiameter;
+                var diameterVer = diameterTypeVer.BarModelDiameter;
+                var normal = wall.HostInfo.VtZ;
+                foreach (var l in wall.LineCenterHorizontal)
+                {
+                    try
+                    {
+                        var dir = l.Direction.Normalize();
+                        var p1 = l.GetEndPoint(0)
+                            + wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            + dir * (cover + diameter / 2);
+                        var p2 = l.GetEndPoint(0)
+                            - wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2 - diameterVer)
+                            + dir * (cover + diameter / 2);
+                        var p3 = l.GetEndPoint(1)
+                            - wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2 - diameterVer)
+                            - dir * (cover + diameter / 2);
+                        var p4 = l.GetEndPoint(1)
+                            + wall.HostInfo.VtY * (wall.HostInfo.Thickness / 2 - cover - diameter / 2)
+                            - dir * (cover + diameter / 2);
+                        var ps = new List<XYZ>() { p1, p2, p3, p4 };
+                        var cs = ps.PointsToCurves();
+                        var rebar = Rebar.CreateFromCurves(
+                            _cmd.Document,
+                            RebarStyle.Standard,
+                            diameterType,
+                            null,
+                            null,
+                            host,
+                            normal,
+                            cs,
+                            RebarHookOrientation.Left,
+                            RebarHookOrientation.Left,
+                            true, true);
 
-        public void InstallRebarHorizontalNear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void InstallRebarVerticalFar()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void InstallRebarVerticalNear()
-        {
-            throw new NotImplementedException();
+                        if (_cmd.Document.ActiveView is View3D view3d)
+                            rebar.SetSolidInView(view3d, true);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                IO.ShowWarning(ex.Message);
+            }
         }
     }
 }
--- DBimTool/DBimTool/Tools/CreateRebarForMainHoleType1/viewModels/CreateRebarForMainHoleType1ViewModel.cs
+++ DBimTool/DBimTool/Tools/CreateRebarForMainHoleType1/viewModels/CreateRebarForMainHoleType1ViewModel.cs
@@ -13,18 +13,30 @@ namespace DBimTool.Tools.CreateRebarForMainHoleType1.viewModels
         private CreateRebarForMainHoleType1Cmd _cmd;
         private IMainHole1BottomSlabService _mainHole1BottomSlabService;
         private IMainHole1TopSlabService _mainHole1TopSlabService;
+        private IMainHole1Wall1Service _mainHole1Wall1Service;
+        private IMainHole1Wall2Service _mainHole1Wall2Service;
+        private IMainHole1Wall3Service _mainHole1Wall3Service;
+        private IMainHole1Wall4Service _mainHole1Wall4Service;
         public CreateRebarForMainHoleType1ElementInstance ElementInstances { get; set; }
         public CreateRebarForMainHoleType1View MainView { get; set; }
         public CreateRebarForMainHoleType1ViewModel(
             CreateRebarForMainHoleType1Cmd cmd,
             CreateRebarForMainHoleType1ElementInstance elementInstances,
             IMainHole1BottomSlabService mainHole1BottomSlabService,
-            IMainHole1TopSlabService mainHole1TopSlabService)
+            IMainHole1TopSlabService mainHole1TopSlabService,
+            IMainHole1Wall1Service mainHole1Wall1Service,
+            IMainHole1Wall2Service mainHole1Wall2Service,
+            IMainHole1Wall3Service mainHole1Wall3Service,
+            IMainHole1Wall4Service mainHole1Wall4Service)
         {
             _cmd = cmd;
             ElementInstances = elementInstances;
             _mainHole1BottomSlabService = mainHole1BottomSlabService;
             _mainHole1TopSlabService = mainHole1TopSlabService;
+            _mainHole1Wall1Service = mainHole1Wall1Service;
+            _mainHole1Wall2Service = mainHole1Wall2Service;
+            _mainHole1Wall3Service = mainHole1Wall3Service;
+            _mainHole1Wall4Service = mainHole1Wall4Service;
             MainView = new CreateRebarForMainHoleType1View() { DataContext = this };
         }
         [RelayCommand]
@@ -45,6 +57,26 @@ namespace DBimTool.Tools.CreateRebarForMainHoleType1.viewModels
                 _mainHole1TopSlabService.InstallRebarBotY();
                 _mainHole1TopSlabService.InstallRebarTopX();
                 _mainHole1TopSlabService.InstallRebarBotX();
+
+                _mainHole1Wall1Service.InstallRebarVerticalNear();
+                _mainHole1Wall1Service.InstallRebarHorizontalNear();
+                _mainHole1Wall1Service.InstallRebarVerticalFar();
+                _mainHole1Wall1Service.InstallRebarHorizontalFar();
+
+                _mainHole1Wall2Service.InstallRebarVerticalNear();
+                _mainHole1Wall2Service.InstallRebarHorizontalNear();
+                _mainHole1Wall2Service.InstallRebarVerticalFar();
+                _mainHole1Wall2Service.InstallRebarHorizontalFar();
+
+                _mainHole1Wall3Service.InstallRebarVerticalNear();
+                _mainHole1Wall3Service.InstallRebarHorizontalNear();
+                _mainHole1Wall3Service.InstallRebarVerticalFar();
+                _mainHole1Wall3Service.InstallRebarHorizontalFar();
+
+                _mainHole1Wall4Service.InstallRebarVerticalNear();
+                _mainHole1Wall4Service.InstallRebarHorizontalNear();
+                _mainHole1Wall4Service.InstallRebarVerticalFar();
+                _mainHole1Wall4Service.InstallRebarHorizontalFar();
                 //--------
                 ts.Commit();
             }
